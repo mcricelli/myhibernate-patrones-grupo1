a9130b11e4f8be06ed6a0d673ea38efb04245868
Santi871 <santivegega@gmail.com>
2020-05-08 00:50:24 -0300

    segunda implementacion de find: arma un query solo con joins

--- src/myhibernate/MyHibernate.java
+++ src/myhibernate/MyHibernate.java
@@ -1,5 +1,6 @@
 package myhibernate;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,124 +13,145 @@ import java.lang.System;
 import myhibernate.demo.DatabaseManager;
 import myhibernate.ann.*;
 
-public class MyHibernate
-{
-   private static final Class<?> tipoEntero = Integer.TYPE;
+public class MyHibernate {
+    private static final Class<?> tipoEntero = Integer.TYPE;
+    private static final Class<?> tipoDouble = Double.TYPE;
 
-   public static <T> T find(Class<T> clazz, int id)
-   {
-      String nombreColumnaID = "";
-      SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-      T objetoRetorno = null;
-      DatabaseManager db = new DatabaseManager(System.getenv("DB_URL"),"sa", "");
-      db.conectar();
+    public static <T> T find(Class<T> clazz, int id) {
+        String nombreColumnaID = "";
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        T objetoRetorno = null;
+        DatabaseManager db = new DatabaseManager(System.getenv("DB_URL"), "sa", "");
+        db.conectar();
 
-      // generar una instancia nueva de la clase generica
-      try {
-         objetoRetorno = clazz.newInstance();
-      } catch (InstantiationException | IllegalAccessException e) {
-         e.printStackTrace();
-      }
+        // generar una instancia nueva de la clase generica
+        try {
+            objetoRetorno = clazz.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
 
-      // si no tiene la annotation Entity, no esta mapeada la clase
-      assert clazz.isAnnotationPresent(Entity.class);
+        // si no tiene la annotation Entity, no esta mapeada la clase
+        assert clazz.isAnnotationPresent(Entity.class);
 
-      Field[] fields = clazz.getDeclaredFields();
-      String table = clazz.getAnnotation(Table.class).name();
+        Field[] fields = clazz.getDeclaredFields();
+        String table = clazz.getAnnotation(Table.class).name();
+        StringBuilder q = new StringBuilder(String.format("select * from %s", table));
 
-      for(Field field : fields){
-         // recorro los annotations hasta que encuentro el Id
-         if(field.isAnnotationPresent(Id.class))
-            nombreColumnaID = field.getAnnotation(Column.class).name();
-      }
+        for (Field field : fields) {
+            // recorro los annotations hasta que encuentro el Id
+            if (field.isAnnotationPresent(Id.class))
+                nombreColumnaID = field.getAnnotation(Column.class).name();
 
-      // voy a buscar a la db
-      String q = String.format("select * from %s where %s=%s", table, nombreColumnaID, id);
-      ResultSet rs = db.query(q);
-      try {
-         rs.next();
-      } catch (SQLException throwables) {
-         // si el resultset esta vacio, deberia fallar
-         throwables.printStackTrace();
-         db.cerrar();
-         return null;
-      }
+            // si encuentro un JoinColumn, agrego un join al query
+            if (field.isAnnotationPresent(JoinColumn.class)) {
+                String nombreColumnaIDFK = field.getAnnotation(JoinColumn.class).name();
+                Class<?> tipoField = field.getType();
+                String tablaTipoField = tipoField.getAnnotation(Table.class).name();
 
-      for(Field field : fields){
-         // TODO ver ManyToOne
-         // recorro todos los campos
-         Class<?> tipoField = field.getType();
+                q.append(String.format(" JOIN %s ON %s.%s=%s.%s", tablaTipoField, table, nombreColumnaIDFK, tablaTipoField, nombreColumnaIDFK));
+            }
+        }
 
-         if(field.isAnnotationPresent(Column.class)) {
+        // voy a buscar a la db
+        // String q = String.format("select * from %s where %s=%s", table, nombreColumnaID, id);
+        q.append(String.format(" WHERE %s=%s", nombreColumnaID, id));
+        ResultSet rs = db.query(q.toString());
+        try {
+            rs.next();
+        } catch (SQLException throwables) {
+            // si el resultset esta vacio, deberia fallar
+            throwables.printStackTrace();
+            db.cerrar();
+            return null;
+        }
+
+        for (Field field : fields) {
+            // TODO ver ManyToOne
+            // recorro todos los campos
+            Class<?> tipoField = field.getType();
+
+            if (field.isAnnotationPresent(Column.class)) {
+                // caso campo es un Column
+                Object campoObjetoRetorno = obtenerValor(field, rs);
+                try {
+                    field.set(objetoRetorno, campoObjetoRetorno);
+                } catch (IllegalAccessException | IllegalArgumentException e) {
+                    e.printStackTrace();
+                }
+
+            } else if (field.isAnnotationPresent(JoinColumn.class)) {
+                // caso campo es un JoinColumn
+                Object objetoJoin = null;
+                try {
+                    objetoJoin = field.getType().newInstance();
+                    for (Field fieldObjetoJoin : objetoJoin.getClass().getFields()) {
+                        Object campoObjetoJoin = obtenerValor(fieldObjetoJoin, rs);
+                        fieldObjetoJoin.set(objetoJoin, campoObjetoJoin);
+                    }
+                } catch (InstantiationException | IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    field.set(objetoRetorno, objetoJoin);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        db.cerrar();
+        return objetoRetorno;
+    }
+
+    public static <T> List<T> findAll(Class<T> clazz) {
+        // PROGRAMAR AQUI
+        return null;
+    }
+
+    public static Query createQuery(String hql) {
+        // PROGRAMAR AQUI
+        return null;
+    }
+
+    private static Object obtenerValor(Field field, ResultSet rs) {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        Class<?> tipoField = field.getType();
+        Object valorColumna;
+        if (field.isAnnotationPresent(Column.class)) {
             // caso campo es un Column
             String nombreColumna = field.getAnnotation(Column.class).name();
-            int valorColumnaInt;
             String valorColumnaString;
 
             try {
-               if (tipoField.isAssignableFrom(tipoEntero)) {
-                  // el field es tipo int
-                  valorColumnaInt = rs.getInt(nombreColumna);
-                  try {
-                     field.set(objetoRetorno, valorColumnaInt);
-                  } catch (IllegalAccessException e) {
-                     e.printStackTrace();
-                  }
+                if (tipoField.isAssignableFrom(tipoEntero)) {
+                    // el campo es tipo int
+                    valorColumna = rs.getInt(nombreColumna);
+                    return valorColumna;
 
-               } else if (tipoField.isAssignableFrom(String.class)) {
-                  // el campo es tipo string
-                  valorColumnaString = rs.getString(nombreColumna);
-                  try {
-                     field.set(objetoRetorno, valorColumnaString);
-                  } catch (IllegalAccessException e) {
-                     e.printStackTrace();
-                  }
+                } else if (tipoField.isAssignableFrom(String.class)) {
+                    // el campo es tipo string
+                    valorColumna = rs.getString(nombreColumna);
+                    return valorColumna;
 
-               } else if (tipoField.isAssignableFrom(Date.class)) {
-                  // el campo es tipo date
-                  valorColumnaString = rs.getString(nombreColumna);
-                  try {
-                     Date d = df.parse(valorColumnaString);
-                     field.set(objetoRetorno, d);
-                  } catch (ParseException | IllegalAccessException e) {
-                     e.printStackTrace();
-                  }
-               }
+                } else if (tipoField.isAssignableFrom(Date.class)) {
+                    // el campo es tipo date
+                    valorColumnaString = rs.getString(nombreColumna);
+                    try {
+                        return df.parse(valorColumnaString);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                } else if (tipoField.isAssignableFrom(tipoDouble)){
+                    // el campo es tipo double
+                    valorColumna = rs.getDouble(nombreColumna);
+                    return valorColumna;
+                }
             } catch (SQLException throwables) {
-               throwables.printStackTrace();
+                throwables.printStackTrace();
             }
 
-         } else if(field.isAnnotationPresent(JoinColumn.class)){
-            // caso campo es un JoinColumn (hay que encontrar la FK y llamar a find recursivamente)
-            String nombreColumna = field.getAnnotation(JoinColumn.class).name();
-            int foreignKey = -1;
-            try {
-               foreignKey = rs.getInt(nombreColumna);
-            } catch (SQLException throwables) {
-               throwables.printStackTrace();
-            }
-
-            try {
-               field.set(objetoRetorno, find(tipoField, foreignKey));
-            } catch (IllegalAccessException e) {
-               e.printStackTrace();
-            }
-         }
-      }
-      db.cerrar();
-      return objetoRetorno;
-   }
-
-   public static <T> List<T> findAll(Class<T> clazz)
-   {
-      // PROGRAMAR AQUI
-      return null;
-   }
-
-   public static Query createQuery(String hql)
-   {
-      // PROGRAMAR AQUI
-      return null;
-   }
-
+        }
+        return null;
+    }
 }
